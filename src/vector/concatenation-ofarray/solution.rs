e725d1ea34295ef765bac735ab5d2e711eff975d
jinjoolee07 <jinjoolee07@gmail.com>
2023-12-31 19:39:21 +0900

    new solution of concatenation of array

--- src/vector/concatenation-ofarray/solution.rs
+++ src/vector/concatenation-ofarray/solution.rs
@@ -1,5 +1,12 @@
 impl Solution {
     pub fn get_concatenation(nums: Vec<i32>) -> Vec<i32> {
-        nums.repeat(2);
+        //create a new array to store results
+        let mut result = Vec::new();
+
+        for num in nums {
+            result.push(num);
+            result.push(num);
+        }
+        result
     }
 }
